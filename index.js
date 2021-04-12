894f4268fbaa9e13ddcf27c7509f53c2ed7525df
Carlj0666 <carlj1881@yahoo.com>
2021-04-12 13:01:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+function saturdayFun(def = "roller-skate") {
+  return `This Saturday, I want to ${def}!`
+}
+
+let mondayWork = function(def = "go to the office") {
+  return `This Monday, I will ${def}.`
+}
+
+function wrapAdjective(special = "*") {
+  let inner = function(string) {
+    return `You are ${special}${string}${special}!`
+  }
+  return inner
+}
+
+const Calculator = {
+  add: function(num1, num2) {
+    return num1 + num2
+  },
+  subtract: function(num1, num2) {
+    return num1 - num2
+  },
+  multiply: function(num1, num2) {
+    return num1 * num2
+  },
+  divide: function(num1, num2) {
+    return num1 / num2
+  }
+}
+
+
+function actionApplyer(startInt, actions) {
+  function callActionOnAccumulator(accumulator, action) {
+    return action(accumulator)
+  }
+  return actions.reduce(callActionOnAccumulator, startInt)
+}
