d5244ef9befd7bf669bd70b93fc715e3b748458b
Carlj0666 <carlj1881@yahoo.com>
2021-04-12 12:49:56 -0700

    commit tree 5324b5df3025e366f1d42e9c70a500f5fc0895da

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+function saturdayFun(def = "roller-skate") {
+  return `This Saturday, I want to ${def}!`
+}
+
+let mondayWork = function(def = "go to the office") {
+  return `This Monday, I will ${def}.`
+}
+
+function wrapAdjective(special = "*") {
+  let inner = function(string) {
+    return `You are ${special}${string}${special}!`
+  }
+  return inner
+}
+
+const Calculator = {
+  add: function(num1, num2) {
+    return num1 + num2
+  },
+  subtract: function(num1, num2) {
+    return num1 - num2
+  },
+  multiply: function(num1, num2) {
+    return num1 * num2
+  },
+  divide: function(num1, num2) {
+    return num1 / num2
+  }
+}
+
+
+function actionApplyer(startInt, array) {
+  
+  return array.reduce(function(accumulator, innerFunc) {
+    return innerFunc(accumulator)
+  }, startInt
+  )
+
+  // interate over the function in the array
+    //pass in the starting int
+    // return result
+  
+  return startInt
+
+}
